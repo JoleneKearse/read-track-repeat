8c49f0d11e619ee5b32f572e60e408cee3e489c5
JoleneKearse <pharaohnutz@gmail.com>
2024-02-18 16:57:14 -0500

    chore: configure TailwindCSS

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-2xl underline text-slate-600">Read, Track, Remember</h1>
+    </>
+  )
+}
+
+export default Header
