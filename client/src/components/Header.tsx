35202cddbf9c79c63f2af877cb9d602e93f191dd
JoleneKearse <pharaohnutz@gmail.com>
2024-02-18 18:01:59 -0500

    chore: add styles to title

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <>
-      <h1 className="text-3xl">Read, Track, Repeat</h1>
+      <h1 className="text-3xl text-orange-300 font-bold py-10">
+        Read, Track, Repeat
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "orange-100": "#ffc278",
+      "orange-200": "#ff8807",
+      "orange-300": "#ff981a",
+      "orange-500": "#ed5107",
+    },
+    extend: {
+      backgroundImage: {
+        "bg-gradient": "radial-gradient(ellipse at 50% 75%, #7B315E, #311D35)",
+        "orange-gradient": "linear-gradient(to right, #ff8807, #ff981a, #ed5107)",
+      }
+    },
   },
   plugins: [],
 };
