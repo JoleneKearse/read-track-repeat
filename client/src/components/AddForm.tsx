e8ac428f7b4a7d0a1ad764c0ed822b1e58563268
JoleneKearse <pharaohnutz@gmail.com>
2024-02-19 15:03:26 -0500

    chore: add input field

--- client/src/components/AddForm.tsx
+++ client/src/components/AddForm.tsx
@@ -1,5 +1,3 @@
-
-
 const AddForm = () => {
   return (
     <form className="w-5/6 max-w-sm mx-auto">
@@ -12,13 +10,27 @@ const AddForm = () => {
       <select
         name=""
         id="searchMethod"
-        className="bg-orange-100 border border-orange-200 text-gray-900 text-sm tracking-wide rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5"
+        className="bg-orange-100 border border-orange-200 text-purple-700 text-sm tracking-wide rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 mb-8"
       >
         <option selected>Choose search method</option>
         <option value="isbn">ISBN</option>
         <option value="title">Title</option>
         <option value="author">Author</option>
       </select>
+
+      <label
+        htmlFor="searchInput"
+        className="block mb-2 font-bold tracking-wide text-orange-200 text-med dark:text-white"
+      >
+        Book detail
+      </label>
+      <input 
+        type="text" 
+        name="" 
+        id="searchInput"
+        className="block mb-8 p-2.5 bg-orange-100 border border-orange-200 text-gray-900 text-sm tracking-wide rounded-lg w-full placeholder:text-purple-500 focus:ring-purple-500 focus:border-purple-500" 
+        placeholder="ISBN / Title / Author"
+      />
     </form>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,13 +7,16 @@ export default {
       "orange-200": "#ff8807",
       "orange-300": "#ff981a",
       "orange-500": "#ed5107",
+      "purple-100": "#FFF0FF",
       "purple-500": "#311D35",
+      "purple-700": "#7B315E",
     },
     extend: {
       backgroundImage: {
         "bg-gradient": "radial-gradient(ellipse at 50% 75%, #7B315E, #311D35)",
-        "orange-gradient": "linear-gradient(to right, #ff8807, #ff981a, #ed5107)",
-      }
+        "orange-gradient":
+          "linear-gradient(to right, #ff8807, #ff981a, #ed5107)",
+      },
     },
   },
   plugins: [],
