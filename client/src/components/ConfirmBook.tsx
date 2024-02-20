0307edc528db997c1a676862d1fac1aa0ea578f8
JoleneKearse <pharaohnutz@gmail.com>
2024-02-20 13:19:06 -0500

    chore: add ConfirmBook buttons with styles for mob & tablet

--- client/src/components/ConfirmBook.tsx
+++ client/src/components/ConfirmBook.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Book } from "../types";
+import Check from "/check.svg";
+import Cross from "/cross.svg";
 
 interface ConfirmBookProps {
   books: Book[];
@@ -9,7 +11,7 @@ const ConfirmBook: React.FC<ConfirmBookProps> = ({ books }) => {
   return (
     <main className="w-5/6 max-w-sm py-20 mx-auto md:max-w-md">
       {books.map((book) => (
-        <section className="flex flex-col items-center justify-center p-6 bg-purple-700 border border-orange-200 rounded-lg">
+        <form className="flex flex-col items-center justify-center p-6 bg-purple-700 border border-orange-200 rounded-lg">
           <img
             src={book.coverImageUrl}
             alt={book.title}
@@ -31,11 +33,33 @@ const ConfirmBook: React.FC<ConfirmBookProps> = ({ books }) => {
             Published in {book.published}
           </p>
           <p
-            className="text-base tracking-wide text-purple-100"
+            className="pb-10 text-base tracking-wide text-purple-100"
           >
             {book.pages} pages
           </p>
-        </section>
+          <div className="flex ml-[5rem] pb-8 md:ml-[6rem]">
+            <button 
+              type="submit"
+            >
+              <img 
+                src={Cross} 
+                alt="cross" 
+                title="Not my book"
+                className="w-1/3 hover:border hover:border-purple-300 hover:border-4 hover:rounded-full" 
+              />
+            </button>
+            <button 
+              type="submit"
+            >
+              <img 
+                src={Check} 
+                alt="check" 
+                title="Add to collection"
+                className="w-1/3 hover:border hover:border-purple-300 hover:border-4 hover:rounded-full" 
+              />
+            </button>
+          </div> 
+        </form>
       ))}
     </main>
   );
