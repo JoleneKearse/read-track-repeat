9c655e8e0471881938768dd1c98ee1096cb98a21
JoleneKearse <pharaohnutz@gmail.com>
2024-02-29 16:08:53 -0500

    chore: add visual confirmation book added to library

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,13 +1,17 @@
 import React, { useRef, useState, FormEvent } from "react";
 import { Book } from "../types";
 import NoBook from "/not-found.svg";
+import Library from "/library.svg";
 
 interface NotFoundProps {
   handleManuallyAddBook: (newBook: Book) => void;
   addBook: boolean;
 }
 
-const NotFound: React.FC<NotFoundProps> = ({ handleManuallyAddBook, addBook }) => {
+const NotFound: React.FC<NotFoundProps> = ({
+  handleManuallyAddBook,
+  addBook,
+}) => {
   const formData = {
     title: "",
     author: "",
@@ -36,13 +40,24 @@ const NotFound: React.FC<NotFoundProps> = ({ handleManuallyAddBook, addBook }) =
   return (
     <section className="w-5/6 max-w-sm py-20 mx-auto md:max-w-md">
       <article className="flex flex-col items-center justify-center p-6 bg-purple-700 border border-orange-200 rounded-lg">
-        <img src={NoBook} alt="not found" />
-        <p className="text-xl tracking-wide text-orange-100">
-          Sorry, we couldn't find your book!
-        </p>
-        <p className="text-xl tracking-wide text-orange-100">
-          But you can still add the details.
-        </p>
+        {addBook ? (
+          <>
+            <img src={NoBook} alt="not found" />
+            <p className="text-xl tracking-wide text-orange-100">
+              Sorry, we couldn't find your book!
+            </p>
+            <p className="text-xl tracking-wide text-orange-100">
+              But you can still add the details.
+            </p>
+          </>
+        ) : (
+          <>
+            <img src={Library} alt="added to library" />
+            <p className="text-xl tracking-wide text-orange-100">
+              Added to your collection!
+            </p>
+          </>
+        )}
       </article>
 
       {addBook && (
