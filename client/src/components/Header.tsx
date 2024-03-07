8a76a1a9dd61d4868f8f2042d8219a700c7de340
JoleneKearse <pharaohnutz@gmail.com>
2024-03-07 13:45:41 -0500

    add text gradient to title

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="w-5/6 max-w-sm mx-auto md:max-w-md">
-      <h1 className="py-10 text-3xl font-bold text-center text-orange-300 md:text-4xl lg:text-5xl">
-        Read, Track, Repeat
+      <h1 className="py-10 text-3xl font-bold text-center md:text-4xl lg:text-5xl">
+        <span className="text-orange-200a bg-gradient-to-r from-orange-500 to-orange-300 bg-clip-text">Read, Track, Repeat</span>
       </h1>
     </header>
   );
